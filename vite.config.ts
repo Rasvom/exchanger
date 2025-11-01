8903502a3c62eba7e02451d0c2019e72560e7669
Мовсар Бациев <rasvomb@gmail.com>
2025-11-01 10:48:23 +0300

    update chunk size

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,15 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          ui: ['@mantine/core', '@mantine/hooks', '@mantine/form'],
+        },
+      },
+    },
+  },
 });
